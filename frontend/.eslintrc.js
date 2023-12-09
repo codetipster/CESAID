c4903e582555b84db0ea9a4832d603d67fcffc15
Codetipster <talk2samuelnzekwe@gmail.com>
2023-12-09 19:51:13 +0100

    restructure folder

---
+++ frontend/.eslintrc.js
@@ -0,0 +1,24 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+    node: true,
+  },
+  extends: [
+    "eslint:recommended",
+    "plugin:react/recommended",
+    "plugin:prettier/recommended", // Add this line
+  ],
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 12,
+    sourceType: "module",
+  },
+  plugins: ["react", "prettier"], // Add 'prettier' here
+  rules: {
+    "prettier/prettier": "error",
+    // Add other custom rules here
+  },
+};
